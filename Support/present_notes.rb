c173651380d3ab60fe26b24bc1ddca0ed1478b30
Greg Hewett <glhewett@gmail.com>
2009-03-03 20:44:26 -0600

    update to print notes

--- Support/present_notes.rb
+++ Support/present_notes.rb
@@ -9,6 +9,7 @@ project_directory = ENV['TM_PROJECT_DIRECTORY']
 active_filename = ENV['TM_FILENAME']
 active_line_number = ENV['TM_LINE_NUMBER'].to_i 
 default_layout = File.read(File.join(ENV['TM_BUNDLE_SUPPORT'], "layouts", "default.html.erb"))
+selected_tag = ARGV[0] if ARGV 
   
 File.open(active_filename) do |handle|
   note_report = []
@@ -33,8 +34,10 @@ File.open(active_filename) do |handle|
     # end the current note and push on to array to print out.
     if not current_note.nil? and closing = /^----\s*/.match(line)
       current_note[:end_line] = current_line
-      
-      if active_line_number <= current_note[:end_line] and active_line_number >= current_note[:start_line]
+
+      if selected_tag and current_note[:tags].include?(selected_tag)
+        note_report << current_note
+      elsif !selected_tag and active_line_number <= current_note[:end_line] and active_line_number >= current_note[:start_line]
         note_report << current_note
       end
       current_note = nil         
@@ -45,19 +48,25 @@ File.open(active_filename) do |handle|
     unless current_note.nil?
       if tags = /^\s*\[(.*)\]\s*$/.match(line)
         current_note[:tags] << tags[1].split
+        current_note[:tags].flatten!
       else
         current_note[:body] << line
       end
     end
     
   end
-  
+   
   note_report.sort {|a, b| a[:date] <=> b[:date]}.each do |note|
     markdown_report = "\# #{note[:date]}\n\#\# #{note[:title]}\n"
     markdown_report << note[:body].strip
     markdown_report << "\n\n"                    
-    markdown_report << "## Tags\n#{note[:tags].flatten.map {|tag| "* " + tag.downcase + "\n" }}"
-    @document_name = note[:date]
+    markdown_report << "## Tags\n#{note[:tags].map {|tag| "* " + tag.downcase + "\n" }}" 
+    
+    if selected_tag
+      @document_name = "Tag: #{selected_tag}"
+    else 
+      @document_name = note[:date]
+    end
     @document_content = RDiscount.new(markdown_report).to_html
     puts ERB.new(default_layout).result
   end
